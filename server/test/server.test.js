99d90fce789492ea8eb71138d0d57bd3685e6fc2
l am not a Computer Scientist <david.c.h.cao@gmail.com>
2020-07-04 01:28:54 -0300

    Update test cases

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,40 +1,53 @@
-const request = require('supertest')
-const { app } = require('../server');
+const request = require("supertest");
+const { app } = require("../server");
+const sampleCity = "Buenos Aires";
 
-describe('Sample Test', () => {
-    it('should test that true === true', () => {
-      expect(true).toBe(true)
-    })
-  })
-  
+const checkWeatherEntity = (weatherResponse) => {
+  let weatherProps = ["main", "description", "icon", "temp", "iconID"];
+  expect(weatherResponse).toHaveProperty("weather");
+  weatherProps.forEach((prop) =>
+    expect(weatherResponse.weather).toHaveProperty(prop)
+  );
+};
 
-  test('Status OK', async () => {
-    const response = await request(app).get('/status');
-    expect(response.statusCode).toBe(200);
-   });
+const checkForecastEntity = (forecastResponse) => {
+  expect(forecastResponse).toHaveProperty("forecast");
+  expect(forecastResponse.forecast.length).toEqual(40);
+  forecastResponse.forecast.forEach((weather) => checkWeatherEntity(weather));
+};
+test("Status OK", async () => {
+  const response = await request(app).get("/status");
+  expect(response.statusCode).toBe(200);
+});
 
-  test('Location OK', async () => {
-    const response = await request(app).get('/v1/location');
-    expect(response.statusCode).toBe(200);
-    expect(response.body.city).toBe('Buenos Aires')
-   });
+test("Location OK", async () => {
+  const response = await request(app).get("/v1/location");
+  expect(response.statusCode).toBe(200);
+  expect(response.body.city).toBe(sampleCity);
+});
 
-  test('Current local weather OK', async () => {
-    const response = await request(app).get('/v1/current/Buenos Aires');
-    expect(response.statusCode).toBe(200);
-   });
+test("Current local weather OK", async () => {
+  const response = await request(app).get("/v1/current/" + sampleCity);
+  expect(response.statusCode).toBe(200);
 
-   test('Current weather by city OK', async () => {
-    const response = await request(app).get('/v1/current/Buenos Aires');
-    expect(response.statusCode).toBe(200);
-   });
+  expect(response.body).toHaveProperty("weather");
+  let weatherResponse = response.body;
+  checkWeatherEntity(weatherResponse);
+});
 
-   test('local forecast OK', async () => {
-    const response = await request(app).get('/v1/forecast/');
-    expect(response.statusCode).toBe(200);
-   });
+test("Current weather by city OK", async () => {
+  const response = await request(app).get("/v1/current/" + sampleCity);
+  expect(response.statusCode).toBe(200);
+});
 
-   test('forecast weather by city OK', async () => {
-    const response = await request(app).get('/v1/forecast/Buenos Aires');
-    expect(response.statusCode).toBe(200);
-   });
+test("local forecast OK", async () => {
+  const response = await request(app).get("/v1/forecast/");
+  expect(response.statusCode).toBe(200);
+  let forecastResponse = response.body;
+  checkForecastEntity(forecastResponse);
+});
+
+test("forecast weather by city OK", async () => {
+  const response = await request(app).get("/v1/forecast/" + sampleCity);
+  expect(response.statusCode).toBe(200);
+});
