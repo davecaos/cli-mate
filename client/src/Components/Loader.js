de7ead8ec06f59dbb40a394f34b865c6b858f643
l am not a Computer Scientist <david.c.h.cao@gmail.com>
2020-07-06 02:22:29 -0300

    Add Loader component

---
+++ client/src/Components/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Loader extends React.Component {
+  render() {
+    return (
+      <>
+        <p>Loading ...</p>
+      </>
+    );
+  }
+}
+
+export default Loader;
